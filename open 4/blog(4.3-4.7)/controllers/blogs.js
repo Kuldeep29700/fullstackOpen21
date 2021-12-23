7beba09b408f1b3a2046860b3ed983402217b22d
KULDEEP KUMAWAT <86726036+Kuldeep29700@users.noreply.github.com>
2021-12-23 21:41:26 +0530

    Add files via upload

---
+++ open 4/blog(4.3-4.7)/controllers/blogs.js
@@ -0,0 +1,27 @@
+const blogsRouter=require('express').Router()
+const Blog=require('../models/blog')
+
+blogsRouter.get('/',(request,response)=>{
+  Blog.find({}).then(blog=>{
+    response.json(blog)
+  })
+    .catch(error => next(error))
+})
+blogsRouter.post('/',(request,response,next)=>{
+  const body= request.body
+  const blog= new Blog({
+    title:body.title,
+    author:body.author,
+    url:body.url,
+    likes:body.like
+  })
+  blog.save()
+    .then(savedBlog=>{
+      response.json(savedBlog)
+    })
+    .catch(error=>next(error))
+})
+
+module.exports= blogsRouter
+
+
